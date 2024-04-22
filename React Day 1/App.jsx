6e27c18e86f04c43ba8f22bf75ff8a4ee997b371
BlurredGirl <163426219+BlurredGirl@users.noreply.github.com>
2024-04-22 11:58:38 +0100

    Basic React Set Up

---
+++ React Day 1/App.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function App() {
+  return (
+    <div>
+      <h1>Hello, world! This is a test</h1>
+      <h2>And this is another test</h2>
+    </div>
+  );
+}
